1758cbc8888515e19aac03f6d95a7462fe6503e1
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-08-06 13:47:36 +0900

    [feat] daisyUI v4 설치 및 커스텀 테마 적용 (#8)
    * chore: daisyUI v4 패키지 설치(#5)
    
    * feat: daisyUI v4 tailwind.config.js에 플러그인 추가(#5)
    
    * feat: daisyUI v4 tailwind.config.js에 커스텀 테마 적용- oz_dark, oz_light(#5)
    
    * feat: oz_dark 테마 index.html의 html 태그 내 data-theme 하드코딩(#5)
    
    * feat: 커스텀 테마 피그마에 맞게 수정- oz_dark, oz_light(#5)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,57 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        oz_dark: {
+          primary: "#6201E0",
+          "primary-hover": "#7B2FF2",
+          "primary-light": "#8E64E5",
+          accent: "#C2A9FF",
+          container: "#1E1E1E",
+          background: "#121212",
+          border: "#2C2C2C",
+
+          "text-primary": "#F5F5F5",
+          "text-dim": "#BBBBBB",
+          "text-reverse": "#1A1A1A",
+
+          info: "#9C6BFF",
+          "info-content": "#1A0F2F",
+          success: "#00FF00",
+          "success-content": "#001600",
+          warning: "#FFCC00",
+          "warning-content": "#332B00",
+          error: "#FF0000",
+          "error-content": "#160000",
+        },
+      },
+      {
+        oz_light: {
+          primary: "#6201E0",
+          "primary-hover": "#5312C6",
+          "primary-light": "#E8DDFD",
+          accent: "#9C6BFF",
+          container: "#FFFFFF",
+          background: "#F9F9FB",
+          border: "#DCDCDC",
+
+          "text-primary": "#1A1A1A",
+          "text-dim": "#666666",
+          "text-reverse": "#F5F5F5",
+
+          info: "#9C6BFF",
+          "info-content": "#FFFFFF",
+          success: "#00FF00",
+          "success-content": "#001600",
+          warning: "#FFCC00",
+          "warning-content": "#332B00",
+          error: "#FF0000",
+          "error-content": "#160000",
+        },
+      },
+    ],
+  },
 };
