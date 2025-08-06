1bcbca44e63b6fca863e705f0a104bf4d34a7006
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-08-06 14:31:17 +0900

    [feat] 전역 폰트 설정(프리텐다드) + 프로젝트 화면 초기화 (#9)
    * feat: Pretendard web font 연결(html)(#6)
    
    * init: main.tsx 초기화(#6)
    
    * init: App.tsx 초기화(#6)
    
    * init: App.css 삭제(#6)
    
    * init: index.css 초기화(#6)
    
    * init: public, assets 디렉토리 초기화(#6)
    
    * feat: 기본 sans를 Pretendard로 설정- tailwind.config.js(#6)
    
    * feat: layer base html font-family도 Pretendard로 설정- index.css(#6)
    
    * chore: vercel 가동 트리거(#6)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Pretendard", "sans-serif", "system-ui"],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
