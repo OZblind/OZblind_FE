e99d6d74cd81d10e2b51e2466cef19988d9a2578
wndud <ju03209@naver.com>
2025-08-06 19:24:50 +0900

    feat: asset 디렉토리에 index.ts 파일 추가(#7)

---
+++ src/assets/index.ts
@@ -0,0 +1,56 @@
+// Logo
+export { default as logoColor } from "./logos/logo-color.svg";
+export { default as logoColorV2 } from "./logos/logo-color-v2.svg";
+export { default as logoDark } from "./logos/logo-dark.svg";
+export { default as logoDarkV2 } from "./logos/logo-dark-v2.svg";
+export { default as logoLight } from "./logos/logo-light.svg";
+export { default as logoLightV2 } from "./logos/logo-light-v2.svg";
+
+// Icons
+export { default as iconArrowUpwardDark } from "./icons/icon-arrow-upward-dark.svg";
+export { default as iconArrowUpwardLight } from "./icons/icon-arrow-upward-light.svg";
+
+export { default as iconBookmarkColor } from "./icons/icon-bookmark-color.svg";
+export { default as iconBookmarkDark } from "./icons/icon-bookmark-dark.svg";
+export { default as iconBookmarkLight } from "./icons/icon-bookmark-light.svg";
+
+export { default as iconDeleteDark } from "./icons/icon-delete-dark.svg";
+export { default as iconDeleteLight } from "./icons/icon-delete-light.svg";
+export { default as iconDelete } from "./icons/icon-delete.svg";
+
+export { default as iconFilterListDark } from "./icons/icon-filter-list-dark.svg";
+export { default as iconFilterListLight } from "./icons/icon-filter-list-light.svg";
+
+export { default as iconKeyboardArrowDownDark } from "./icons/icon-keyboard-arrow-down-dark.svg";
+export { default as iconKeyboardArrowDownLight } from "./icons/icon-keyboard-arrow-down-light.svg";
+export { default as iconKeyboardArrowUpDark } from "./icons/icon-keyboard-arrow-up-dark.svg";
+export { default as iconKeyboardArrowUpLight } from "./icons/icon-keyboard-arrow-up-light.svg";
+
+export { default as iconLabelDark } from "./icons/icon-label-dark.svg";
+export { default as iconLabelLight } from "./icons/icon-label-light.svg";
+
+export { default as iconLogoutBtnHoverDark } from "./icons/icon-logout-btn-hover-dark.svg";
+export { default as iconLogoutBtnHoverLight } from "./icons/icon-logout-btn-hover-light.svg";
+export { default as iconLogoutBtn } from "./icons/icon-logout-btn.svg";
+
+export { default as iconMenuDark } from "./icons/icon-menu-dark.svg";
+export { default as iconMenuLight } from "./icons/icon-menu-light.svg";
+
+export { default as iconNotificationsDark } from "./icons/icon-notifications-dark.svg";
+export { default as iconNotificationsLight } from "./icons/icon-notifications-light.svg";
+export { default as iconNotificationsOnDark } from "./icons/icon-notifications-on-dark.svg";
+export { default as iconNotificationsOnLight } from "./icons/icon-notifications-on-light.svg";
+
+export { default as iconSearch } from "./icons/icon-search.svg";
+
+export { default as iconThumbDownColor } from "./icons/icon-thumb-down-color.svg";
+export { default as iconThumbDownDark } from "./icons/icon-thumb-down-dark.svg";
+export { default as iconThumbDownLight } from "./icons/icon-thumb-down-light.svg";
+
+export { default as iconThumbUpColor } from "./icons/icon-thumb-up-color.svg";
+export { default as iconThumbUpDark } from "./icons/icon-thumb-up-dark.svg";
+export { default as iconThumbUpLight } from "./icons/icon-thumb-up-light.svg";
+
+// Images
+export { default as person } from "./images/person.png";
+export { default as settings } from "./images/settings.png";
