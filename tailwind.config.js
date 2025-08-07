7089803de6d26bc8bcde27eb6396fe5d97d1770a
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-08-07 15:32:51 +0900

    [feat] 컨펌 모달 UI 컴포넌트 구현 (#23)
    * feat: ConfirmModal.tsx 파일 생성(#12)
    
    * fix: 커스텀 테마 변수명 데이지 ui에 맞게 수정(#12)
    
    * fix: 커스텀 테마 변수명 데이지 ui에 맞게 수정(#12)
    
    * feat: 컨펌 모달 코드 작성(#12)
    
    * test: 테스트 페이지 생성 + 컨펌 모달 테스트(#12)
    
    * refactor: 불필요한 useRef 삭제(#12)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -13,23 +13,31 @@ export default {
       {
         oz_dark: {
           primary: "#6201E0",
-          "primary-hover": "#7B2FF2",
-          "primary-light": "#8E64E5",
-          accent: "#C2A9FF",
-          container: "#1E1E1E",
-          background: "#121212",
-          border: "#2C2C2C",
+          "primary-content": "#F5F5F5",
 
-          "text-primary": "#F5F5F5",
-          "text-dim": "#BBBBBB",
-          "text-reverse": "#1A1A1A",
+          secondary: "#7B2FF2",
+          "secondary-content": "#8E64E5",
+
+          accent: "#C2A9FF",
+          "accent-content": "#1A1A1A",
+
+          neutral: "#2C2C2C",
+          "neutral-content": "#BBBBBB",
+
+          "base-100": "#121212",
+          "base-200": "#1E1E1E",
+          "base-300": "#2C2C2C",
+          "base-content": "#F5F5F5",
 
           info: "#9C6BFF",
           "info-content": "#1A0F2F",
+
           success: "#00FF00",
           "success-content": "#001600",
+
           warning: "#FFCC00",
           "warning-content": "#332B00",
+
           error: "#FF0000",
           "error-content": "#160000",
         },
@@ -37,23 +45,31 @@ export default {
       {
         oz_light: {
           primary: "#6201E0",
-          "primary-hover": "#5312C6",
-          "primary-light": "#E8DDFD",
-          accent: "#9C6BFF",
-          container: "#FFFFFF",
-          background: "#F9F9FB",
-          border: "#DCDCDC",
+          "primary-content": "#1A1A1A",
 
-          "text-primary": "#1A1A1A",
-          "text-dim": "#666666",
-          "text-reverse": "#F5F5F5",
+          secondary: "#5312C6",
+          "secondary-content": "#E8DDFD",
+
+          accent: "#9C6BFF",
+          "accent-content": "#FFFFFF",
+
+          neutral: "#DCDCDC",
+          "neutral-content": "#DCDCDC",
+
+          "base-100": "#F9F9FB",
+          "base-200": "#FFFFFF",
+          "base-300": "#DCDCDC",
+          "base-content": "#1A1A1A",
 
           info: "#9C6BFF",
           "info-content": "#FFFFFF",
+
           success: "#00FF00",
           "success-content": "#001600",
-          warning: "#FFCC00",
-          "warning-content": "#332B00",
+
+          warning: "#00FF00",
+          "warning-content": "#001600",
+
           error: "#FF0000",
           "error-content": "#160000",
         },
