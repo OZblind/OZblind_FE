123846dc7c2bada201bd2b960068bf1894f12170
Makee <ina0312@naver.com>
2025-08-07 16:22:32 +0900

    feat: Toast 컨테이너(스택 관리용) 코드 작성(#13)

--- src/components/commons/Toast/ToastContainer.tsx
+++ src/components/commons/Toast/ToastContainer.tsx
@@ -1,1 +1,13 @@
-export function ToastContainer() {}
+import type { ReactNode } from "react";
+
+interface ToastContainerProps {
+  children: ReactNode;
+}
+
+export default function ToastContainer({ children }: ToastContainerProps) {
+  return (
+    <div className="fixed top-6 left-1/2 transform -translate-x-1/2 z-[5000] pointer-events-none">
+      <div className="flex flex-col gap-3">{children}</div>
+    </div>
+  );
+}
