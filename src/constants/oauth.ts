c378488dd637b4bbb94a0b23817387f4adf80db1
Makee <ina0312@naver.com>
2025-08-08 15:35:31 +0900

    feat: oauth.ts env 반영하여 API_SIGNUP_URL 추가(#30)

--- src/constants/oauth.ts
+++ src/constants/oauth.ts
@@ -2,3 +2,4 @@ export const OAUTH_START_URL = import.meta.env.VITE_OAUTH_START_URL as string;
 export const OAUTH_REDIRECT_URI = import.meta.env
   .VITE_OAUTH_REDIRECT_URI as string;
 export const API_CALLBACK_URL = import.meta.env.VITE_API_CALLBACK_URL as string;
+export const API_SIGNUP_URL = import.meta.env.VITE_API_SIGNUP_URL as string;
