8524aeabe8926a47d4fa9ecd21149cded3a4b39e
L <lmwoo0327@naver.com>
2025-08-08 17:15:44 +0900

    feat: TestHub 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import ForbiddenPage from "./pages/error/403";
 import NotFoundPage from "./pages/error/404";
 import ServerErrorPage from "./pages/error/500";
 import TestPostWritePage from "./pages/test/TestPostWritePage";
+import TestHub from "./pages/test/TestHub";
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<TestPostWritePage />} />
+      {/* 메인에서 테스트 허브 보여주기 */}
+      <Route path="/" element={<TestHub />} />
+
+      {/* 테스트 페이지들 */}
+      <Route path="/test/write" element={<TestPostWritePage />} />
+
       {/* 에러 페이지 */}
       <Route path="/403" element={<ForbiddenPage />} />
       <Route path="/500" element={<ServerErrorPage />} />
+
+      {/* 404 */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
