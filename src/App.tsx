88a031f0cfa8cf553eea7cde232331ddbf2a1bc3
people9953 <people99532@gmail.com>
2025-08-08 13:59:25 +0900

    feat : MyPageLayout 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import ForbiddenPage from "./pages/error/403";
 import NotFoundPage from "./pages/error/404";
 import ServerErrorPage from "./pages/error/500";
+import MyPageLayout from "./pages/mypage/MyPageLayout";
 
 const App: React.FC = () => {
   return (
     <Routes>
+      <Route path="/mypage" element={<MyPageLayout />}></Route>
       {/* 에러 페이지 */}
       <Route path="/403" element={<ForbiddenPage />} />
       <Route path="/500" element={<ServerErrorPage />} />
