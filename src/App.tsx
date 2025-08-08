03a9054fb36ceedd3de992697d6a61eb70592f4b
people9953 <people99532@gmail.com>
2025-08-08 11:10:44 +0900

    refactor : BrowserRouter를 main.tsx이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ForbiddenPage from "./pages/error/403";
 import NotFoundPage from "./pages/error/404";
 import ServerErrorPage from "./pages/error/500";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 에러 페이지 */}
-        <Route path="/403" element={<ForbiddenPage />} />
-        <Route path="/500" element={<ServerErrorPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* 에러 페이지 */}
+      <Route path="/403" element={<ForbiddenPage />} />
+      <Route path="/500" element={<ServerErrorPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
 
