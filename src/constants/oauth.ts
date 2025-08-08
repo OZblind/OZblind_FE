462cc27babe945dca467bad90a230a6cad4aea67
Makee <ina0312@naver.com>
2025-08-08 14:35:45 +0900

    feat: oauth 관련 상수 constants 폴더 내 추가(#30)

---
+++ src/constants/oauth.ts
@@ -0,0 +1,3 @@
+export const OAUTH_START_URL = import.meta.env.VITE_OAUTH_START_URL;
+export const OAUTH_REDIRECT_URI = import.meta.env.VITE_OAUTH_REDIRECT_URI;
+export const API_CALLBACK_URL = import.meta.env.VITE_API_CALLBACK_URL;
