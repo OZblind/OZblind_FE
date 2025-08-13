28eda660865f6c8baaa3587b8f7852f5a32c5dee
wndud <ju03209@naver.com>
2025-08-13 22:22:17 +0900

    feat: 사용자 설정 테스트 페이지 구현(#29)

---
+++ src/pages/test/TestSettingPage.tsx
@@ -0,0 +1,26 @@
+import { SettingsPage } from "@components/commons/SettingModal/SettingsPage";
+import { ThemeInitializer } from "@src/components/commons/SettingModal/ThemeInitializer";
+import { useState } from "react";
+
+export default function TestSettingPage() {
+  const [settingOpen, setSettingIsOpen] = useState<boolean>(false);
+  const [theme, setTheme] = useState<"oz_dark" | "oz_light">("oz_dark");
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <button
+        type="button"
+        onClick={() => setSettingIsOpen(true)}
+        className="rounded-full border w-40 h-40 text-center"
+      >
+        프로필
+      </button>
+      <SettingsPage
+        isOpen={settingOpen}
+        setIsOpen={setSettingIsOpen}
+        theme={theme}
+        setTheme={setTheme}
+      />
+      <ThemeInitializer setTheme={setTheme} />
+    </div>
+  );
+}
