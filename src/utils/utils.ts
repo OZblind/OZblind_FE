423380532649e7a8af87d786fa3cc75831582c92
L <lmwoo0327@naver.com>
2025-08-13 16:32:02 +0900

    feat: utils 에 날짜 포멧팅 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,17 @@
 export function cn(...classes: (string | false | null | undefined)[]) {
   return classes.filter(Boolean).join(" ");
 }
+
+export const fmtDate = (iso: string) =>
+  new Date(iso).toLocaleString(undefined, {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+export const fmtNum = (n: number) =>
+  new Intl.NumberFormat(undefined, {
+    notation: n > 999 ? "compact" : "standard",
+  }).format(n);
