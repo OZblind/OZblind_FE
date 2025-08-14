0fe3eb729c4c0739dd28e0e6dc69731918099cbe
wndud <ju03209@naver.com>
2025-08-14 18:34:18 +0900

    feat: 메인 테스트 페이지 생성(#17)

---
+++ src/pages/test/TestMainPage.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@src/components/commons/Sidebar/Sidebar";
+
+export default function TestMainPage() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      메인이에용
+    </div>
+  );
+}
