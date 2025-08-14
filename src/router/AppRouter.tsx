f2f36c29984f5131a2d77f86ab8d33c0629ff3f8
wndud <ju03209@naver.com>
2025-08-14 18:36:20 +0900

    feat: 테스트 페이지 라우트 연결(#17)

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -32,6 +32,7 @@ import {
 } from "@hooks/useAuthQueries";
 import { useToastStore } from "@store/toastStore";
 import { resolvePostLoginPath } from "@utils/postLogin";
+import TestMainPage from "@src/pages/test/TestMainPage";
 
 /** 공개(보호 불필요) 경로 목록 */
 const PUBLIC_PATHS: ReadonlySet<string> = new Set([
@@ -189,6 +190,7 @@ export default function AppRouter() {
         <Route path={PATHS.ROOT} element={<TestHub />} />
         <Route path="/test/write" element={<TestPostWritePage />} />
         <Route path="/test/setting" element={<TestSettingPage />} />
+        <Route path="/test/main" element={<TestMainPage />}></Route>
 
         {/* 인증 로비(공개) */}
         <Route path={PATHS.AUTH} element={<LandingPage />} />
