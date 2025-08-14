d8cbc965be2b27300dd26558a2a69e76326d4ddf
wndud <ju03209@naver.com>
2025-08-14 11:50:53 +0900

    feat: 키 미인증 시 닫기 버튼 클릭 방지(#29)

--- src/components/commons/SettingModal/SettingsPopup.tsx
+++ src/components/commons/SettingModal/SettingsPopup.tsx
@@ -12,11 +12,15 @@ type Props = {
 };
 
 export function SettingPopup({ setIsOpen, theme, setTheme }: Props) {
+  const isAuthenticated = true; // 오즈키 인증 상태(임시)
   return (
     <div className="relative bg-base-200 rounded-md p-6 max-w-md w-full h-[500px]">
       <button
         className="absolute top-4 right-4 px-4 text-base-content py-2 rounded-full hover:bg-base-300 transition"
-        onClick={() => setIsOpen(false)}
+        onClick={() => {
+          if (!isAuthenticated) return;
+          setIsOpen(false);
+        }}
       >
         X
       </button>
