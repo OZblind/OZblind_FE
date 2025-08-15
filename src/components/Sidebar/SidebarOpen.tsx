03b73cd0888f44a0952c71805dd064ec2fed5af0
wndud <ju03209@naver.com>
2025-08-16 06:27:16 +0900

    feat: 알림모달 연결(#17)

--- src/components/Sidebar/SidebarOpen.tsx
+++ src/components/Sidebar/SidebarOpen.tsx
@@ -1,7 +1,10 @@
 import { icons } from "@src/assets";
 import { useThemeIcon } from "@hooks/useThemeIcon";
+import { NotificationModal } from "@components/Notice";
+import { useState } from "react";
 
 export function SidebarOpen({ onToggle }: { onToggle: () => void }) {
+  const [noticeOpen, setNoticeOpen] = useState(false);
   const themeIcon = useThemeIcon();
   const menuIcon = themeIcon === "oz_dark" ? icons.menu.dark : icons.menu.light;
   const notificationsIcon =
@@ -11,8 +14,12 @@ export function SidebarOpen({ onToggle }: { onToggle: () => void }) {
 
   return (
     <div className="flex flex-col items-center w-full h-full bg-base-200 p-6">
+      <NotificationModal
+        open={noticeOpen}
+        onClose={() => setNoticeOpen(false)}
+      />
       <div className="flex justify-between w-[260px] h-[24px]">
-        <button onClick={onToggle}>
+        <button onClick={() => setNoticeOpen(true)}>
           <img src={notificationsIcon} alt="notificationsIcon" />
         </button>
         <button onClick={onToggle}>
