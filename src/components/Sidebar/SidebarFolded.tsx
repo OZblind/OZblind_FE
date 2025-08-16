43255b4d92b21d82f50f5766a1705d5496558db4
wndud <ju03209@naver.com>
2025-08-17 01:56:42 +0900

    feat: 닫힌 사이드 바에 알림, 프로필 버튼 추가(#17)

--- src/components/Sidebar/SidebarFolded.tsx
+++ src/components/Sidebar/SidebarFolded.tsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import { icons } from "@src/assets";
 import { useThemeIcon } from "@hooks/useThemeIcon";
+import profile from "@assets/images/profile.jpg";
+import { NotificationModal } from "@components/Notice";
 
 export function SidebarFolded({ onToggle }: { onToggle: () => void }) {
+  const [noticeOpen, setNoticeOpen] = useState(false);
   const themeIcon = useThemeIcon();
   const menuIcon = themeIcon === "oz_dark" ? icons.menu.dark : icons.menu.light;
+  const notificationsIcon =
+    themeIcon === "oz_dark"
+      ? icons.notifications.dark
+      : icons.notifications.light;
 
   return (
     <div className="flex flex-col items-center justify-between w-full h-full bg-base-200 py-6">
       <button onClick={onToggle}>
         <img src={menuIcon} alt="menuIcon" />
       </button>
+      <div className="flex flex-col gap-4 items-center">
+        <NotificationModal
+          open={noticeOpen}
+          onClose={() => setNoticeOpen(false)}
+        />
+        <button onClick={() => setNoticeOpen(true)}>
+          <img src={notificationsIcon} alt="notificationsIcon" />
+        </button>
+        <button>
+          <div className="flex justify-center items-center w-10 h-10 rounded-full hover:bg-base-300 hover:scale-110">
+            <img src={profile} alt="profile" className="w-8 h-8 rounded-full" />
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
