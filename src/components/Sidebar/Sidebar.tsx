08a7c5bb8e0b42aabbfbe41a20f75937f3baaa73
wndud <ju03209@naver.com>
2025-08-17 01:25:10 +0900

    feat: 창 크기에 따른 사이드 바 열림 여부(#17)

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SidebarOpen } from "./SidebarOpen";
 import { SidebarFolded } from "./SidebarFolded";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
+
+  // 창 크기에 따른 사이드 바 열림 여부
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1200) {
+        setIsOpen(false);
+      } else {
+        setIsOpen(true);
+      }
+    };
+
+    handleResize(); // 첫 렌더링 시 실행
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div
