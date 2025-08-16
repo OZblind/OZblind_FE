28e251a46de07c702494304fa2bac1430e8d0a83
wndud <ju03209@naver.com>
2025-08-17 01:44:09 +0900

    fix: 프로필 박스를 버튼으로 수정(#17)

--- src/components/Sidebar/SmallProfileSection.tsx
+++ src/components/Sidebar/SmallProfileSection.tsx
@@ -5,13 +5,13 @@ export default function SmallProfileSection() {
   return (
     <div className="flex items-center pt-4 pb-2 pr-28">
       <div className="flex justify-center items-center w-[74px] h-[74px] rounded-lg overflow-hidden transform transition-transform duration-300 hover:bg-base-300 hover:scale-105">
-        <div className="relative w-[60px] h-[60px]">
+        <button className="relative w-[60px] h-[60px]">
           <img
             src={profile}
             alt="profile"
             className="w-full h-full object-cover rounded-lg"
           />
-        </div>
+        </button>
       </div>
       <p className="p-2">{userTag}</p>
     </div>
