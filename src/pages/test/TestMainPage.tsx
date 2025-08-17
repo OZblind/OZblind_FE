e7ccc458ed17727a48b27bd43a610d9e643ad2c1
wndud <ju03209@naver.com>
2025-08-17 16:48:19 +0900

    design: 메인 테스트 페이지 레이아웃 변경(#17)

--- src/pages/test/TestMainPage.tsx
+++ src/pages/test/TestMainPage.tsx
@@ -4,7 +4,9 @@ export default function TestMainPage() {
   return (
     <div className="flex">
       <Sidebar />
-      메인이에용
+      <div className="flex items-center justify-center w-9/12">
+        <div className="w-[820px] h-[900px] bg-base-200">메인이에용</div>
+      </div>
     </div>
   );
 }
