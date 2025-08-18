7ff7170eaa9cba93af89e843135a95baf32cf5b5
wnduddlekd <162546405+wnduddlekd@users.noreply.github.com>
2025-08-18 17:47:20 +0900

    [feat] Hot 게시글 컴포넌트 UI 구현 (#78)
    * feat: 테스트 페이지 메인 위치 수정(#65)
    
    * feat: Hot 게시판 예시 데이터 추가(#65)
    
    * feat: Hot 게시글 카드 컴포넌트 추가(#65)
    
    * feat: Hot 게시판 컴포넌트 추가(#65)
    
    * feat: 게시글 종류별 색상 추가(#65)
    
    * feat: Hot 게시판 이름 추가(#65)
    
    * fix: 게시판 이름을 각 게시판 색상으로 변경(#65)

--- src/pages/test/TestMainPage.tsx
+++ src/pages/test/TestMainPage.tsx
@@ -1,11 +1,15 @@
 import Sidebar from "@components/Sidebar/Sidebar";
+import HotBoard from "@src/components/HotBoard/HotBoard";
 
 export default function TestMainPage() {
   return (
-    <div className="flex">
+    <div className="relative flex w-full h-screen overflow-hidden">
       <Sidebar />
-      <div className="flex items-center justify-center w-9/12">
-        <div className="w-[820px] h-[900px] bg-base-200">메인이에용</div>
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex flex-col gap-2 items-center justify-center w-[820px] max-h-[900px]">
+          <div className="w-[800px] h-[180px] bg-base-200">취업배너</div>
+          <HotBoard />
+        </div>
       </div>
     </div>
   );
