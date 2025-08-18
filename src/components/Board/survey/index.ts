12fe25aa541c00a75edc9a178b9a9a08f1032238
Makee <ina0312@naver.com>
2025-08-18 15:29:04 +0900

    feat: survey 쪽 index.ts 추가(#51)

---
+++ src/components/Board/survey/index.ts
@@ -0,0 +1,5 @@
+export { default as SurveyCard } from "./SurveyCard";
+export type { SurveyCardProps, SurveyStatus, Chip } from "./SurveyCard";
+
+export { default as SurveyList } from "./SurveyList";
+export type { SurveyListProps } from "./SurveyList";
