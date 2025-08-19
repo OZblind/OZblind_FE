467381ea5d04e33bb40b7a8f76e7e16b31f39526
wndud <ju03209@naver.com>
2025-08-19 10:22:31 +0900

    feat: 메인 페이지 레이아웃(#80)

--- src/pages/test/TestMainPage.tsx
+++ src/pages/test/TestMainPage.tsx
@@ -3,12 +3,15 @@ import HotBoard from "@src/components/HotBoard/HotBoard";
 
 export default function TestMainPage() {
   return (
-    <div className="relative flex w-full h-screen overflow-hidden">
+    <div className="flex w-full min-h-screen">
       <Sidebar />
-      <div className="flex-1 flex items-center justify-center">
-        <div className="flex flex-col gap-2 items-center justify-center w-[820px] max-h-[900px]">
+      <div className="flex-1 flex justify-center">
+        {/** 메인 페이지 */}
+        <div className="flex flex-col gap-2 items-center justify-center w-[820px] h-full">
+          <div className="w-[800px] h-[60px] bg-base-200 my-8"></div>
           <div className="w-[800px] h-[180px] bg-base-200">취업배너</div>
           <HotBoard />
+          <div className="w-[800px] h-[360px] bg-base-200 pb-8"></div>
         </div>
       </div>
     </div>
