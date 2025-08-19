e0ebe0c8b4cc28f7be25e1e6a8ef342dd3650e81
wndud <ju03209@naver.com>
2025-08-20 04:43:24 +0900

    feat: 닫힌 사이드바 게시판 아이콘 별 라우팅 연결(#99)

--- src/components/Sidebar/FoldedPostListSection.tsx
+++ src/components/Sidebar/FoldedPostListSection.tsx
@@ -1,6 +1,8 @@
 import { icons } from "@src/assets";
 import { useThemeIcon } from "@hooks/useThemeIcon";
 import { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { PATHS } from "@src/constants/paths";
 
 export default function FoldedPostListSection() {
   const themeIcon = useThemeIcon();
@@ -20,19 +22,38 @@ export default function FoldedPostListSection() {
     }, [themeIcon]);
 
   const boardButtons = [
-    { key: "free", icon: freeIcon, alt: "freeIcon" },
-    { key: "jobs", icon: jobsIcon, alt: "jobsIcon" },
-    { key: "information", icon: informationIcon, alt: "informationIcon" },
-    { key: "survey", icon: surveyIcon, alt: "surveyIcon" },
-    { key: "github", icon: githubIcon, alt: "githubIcon" },
+    { key: "free", icon: freeIcon, alt: "freeIcon", path: PATHS.FREE_BOARD },
+    { key: "jobs", icon: jobsIcon, alt: "jobsIcon", path: PATHS.JOBS_BOARD },
+    {
+      key: "information",
+      icon: informationIcon,
+      alt: "informationIcon",
+      path: PATHS.INFORMATION_BOARD,
+    },
+    {
+      key: "survey",
+      icon: surveyIcon,
+      alt: "surveyIcon",
+      path: PATHS.SURVEY_BOARD,
+    },
+    {
+      key: "github",
+      icon: githubIcon,
+      alt: "githubIcon",
+      path: PATHS.GITHUB_BOARD,
+    },
   ];
 
   return (
     <div className="flex flex-col gap-3 p-1 py-2 bg-base-100/80 rounded-md">
-      {boardButtons.map(({ key, icon, alt }) => (
-        <button key={key} className="p-1.5 rounded-full hover:bg-base-300">
+      {boardButtons.map(({ key, icon, alt, path }) => (
+        <Link
+          key={key}
+          to={path}
+          className="p-1.5 rounded-full hover:bg-base-300"
+        >
           <img src={icon} alt={alt} />
-        </button>
+        </Link>
       ))}
     </div>
   );
