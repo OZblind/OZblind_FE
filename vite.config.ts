82bcb300d96d7f34fd3cdeda92e9a9f15e6840ae
L <lmwoo0327@naver.com>
2025-08-21 02:29:24 +0900

    feat: 프록시 서버 종료

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://www.ozboard.shop/",
-        changeOrigin: true,
-        secure: false,
-        // ★ 백엔드가 프로젝트 urls.py에서 path('api/', ...)로 걸려있다면 rewrite 필요 없음
-        // rewrite: (path) => path.replace(/^\/api/, ""),  // ← 백엔드가 /api prefix가 "없을 때만" 켜세요
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://www.ozboard.shop/",
+  //       changeOrigin: true,
+  //       secure: false,
+  //       // ★ 백엔드가 프로젝트 urls.py에서 path('api/', ...)로 걸려있다면 rewrite 필요 없음
+  //       // rewrite: (path) => path.replace(/^\/api/, ""),  // ← 백엔드가 /api prefix가 "없을 때만" 켜세요
+  //     },
+  //   },
+  // },
 });
