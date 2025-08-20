65c7fd5f27ae55c16df84bc58fcfd72fed033d40
wnduddlekd <162546405+wnduddlekd@users.noreply.github.com>
2025-08-20 13:45:46 +0900

    [feat] 게시판 버튼 네비게이션 구현 (#103)
    * feat: PATHS에 게시판 경로 상수 추가(#99)
    
    * feat: 열린 사이드바 게시판 버튼별 라우팅 연결(#99)
    
    * feat: 닫힌 사이드바 게시판 아이콘 별 라우팅 연결(#99)
    
    * feat: 메인 레이아웃 추가(#99)
    
    * feat: 메인페이지 라우트 추가(#99)
    
    * remove: 메인 보호 라우트 삭제(#99)
    
    * feat: 메인페이지 생성(#99)
    
    * fix: 취업 경로 이름 수정(#99)
    
    * feat: 오즈 로고 클릭 시 메인으로 이동(#99)
    
    * revert: 게시판 경로를 원래 주소로 변경(#99)
    
    * fix: PATHS 링크 포함 information을 info로 전부 간소화(#99)
    
    * fix: 변경된 상수이름에 맞게 라우트 코드 수정(#99)

--- src/components/Sidebar/FoldedPostListSection.tsx
+++ src/components/Sidebar/FoldedPostListSection.tsx
@@ -1,38 +1,57 @@
 import { icons } from "@src/assets";
 import { useThemeIcon } from "@hooks/useThemeIcon";
 import { useMemo } from "react";
+import { Link } from "react-router-dom";
+import { PATHS } from "@src/constants/paths";
 
 export default function FoldedPostListSection() {
   const themeIcon = useThemeIcon();
 
-  const { freeIcon, githubIcon, informationIcon, jobsIcon, surveyIcon } =
+  const { freeIcon, githubIcon, infoIcon, jobsIcon, surveyIcon } =
     useMemo(() => {
       const dark = themeIcon === "oz_dark";
       return {
         freeIcon: dark ? icons.free.dark : icons.free.light,
         githubIcon: dark ? icons.github.dark : icons.github.light,
-        informationIcon: dark
-          ? icons.information.dark
-          : icons.information.light,
+        infoIcon: dark ? icons.info.dark : icons.info.light,
         jobsIcon: dark ? icons.jobs.dark : icons.jobs.light,
         surveyIcon: dark ? icons.survey.dark : icons.survey.light,
       };
     }, [themeIcon]);
 
   const boardButtons = [
-    { key: "free", icon: freeIcon, alt: "freeIcon" },
-    { key: "jobs", icon: jobsIcon, alt: "jobsIcon" },
-    { key: "information", icon: informationIcon, alt: "informationIcon" },
-    { key: "survey", icon: surveyIcon, alt: "surveyIcon" },
-    { key: "github", icon: githubIcon, alt: "githubIcon" },
+    { key: "free", icon: freeIcon, alt: "freeIcon", path: PATHS.FREE_BOARD },
+    { key: "jobs", icon: jobsIcon, alt: "jobsIcon", path: PATHS.JOBS_BOARD },
+    {
+      key: "info",
+      icon: infoIcon,
+      alt: "infoIcon",
+      path: PATHS.INFO_BOARD,
+    },
+    {
+      key: "survey",
+      icon: surveyIcon,
+      alt: "surveyIcon",
+      path: PATHS.SURVEY_BOARD,
+    },
+    {
+      key: "github",
+      icon: githubIcon,
+      alt: "githubIcon",
+      path: PATHS.GITHUB_BOARD,
+    },
   ];
 
   return (
     <div className="flex flex-col gap-3 p-1 py-2 bg-base-100/80 rounded-md">
-      {boardButtons.map(({ key, icon, alt }) => (
-        <button key={key} className="p-1.5 rounded-full hover:bg-base-300">
+      {boardButtons.map(({ key, icon, alt, path }) => (
+        <Link
+          key={key}
+          to={path}
+          className="p-1.5 rounded-full hover:bg-base-300"
+        >
           <img src={icon} alt={alt} />
-        </button>
+        </Link>
       ))}
     </div>
   );
