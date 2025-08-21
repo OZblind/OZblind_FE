2464f18c780cf113e23e46e1ea8b16c49f410eb4
wndud <ju03209@naver.com>
2025-08-21 14:24:29 +0900

    fix: 게시판 목록 페이지 라우트 컴포넌트 연결 변경(#121)

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -52,9 +52,6 @@ import SurveyListPage from "@src/pages/boards/SurveyListPage";
 
 import TestGithubList from "@src/pages/test/BoardList/TestGithubList";
 import MainLayout from "@src/layouts/MainLayout";
-import FreeBoard from "@src/pages/boards/FreeBoard";
-import JobsBoard from "@src/pages/boards/JobsBoard";
-import InfoBoard from "@src/pages/boards/InfoBoard";
 import WritePostPage from "@src/components/Board/WritePostPage";
 
 /** 공개(보호 불필요) 경로 목록 */
@@ -303,10 +300,23 @@ export default function AppRouter() {
         {/* 메인 페이지 + 게시글 포함 */}
         <Route element={<MainLayout />}>
           <Route path={PATHS.MAIN} element={<MainPage />} />
-          <Route path={PATHS.FREE_BOARD} element={<FreeBoard />} />
-          <Route path={PATHS.JOBS_BOARD} element={<JobsBoard />} />
-          <Route path={PATHS.INFO_BOARD} element={<InfoBoard />} />
-          <Route path={PATHS.SURVEY_BOARD} element={<TestSurveyList />} />
+          <Route
+            path={PATHS.FREE_BOARD}
+            element={<PostListPage board="free" />}
+          />
+          <Route
+            path={PATHS.JOBS_BOARD}
+            element={<PostListPage board="job" />}
+          />
+          <Route
+            path={PATHS.INFO_BOARD}
+            element={<PostListPage board="info" />}
+          />
+          <Route
+            path={PATHS.SURVEY_BOARD}
+            element={<PostListPage board="survey" />}
+          />
+
           <Route path={PATHS.GITHUB_BOARD} element={<TestGithubList />} />
           <Route path={PATHS.POST_CREATE} element={<WritePostPage />} />
           <Route path={PATHS.POST_DETAIL} element={<PostDetailPage />}></Route>
