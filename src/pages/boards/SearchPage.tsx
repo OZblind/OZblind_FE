4bd9ff5defb1c843ffd687d6be54b14aa79030bb
people9953 <people99532@gmail.com>
2025-08-24 00:26:34 +0900

    feat:수정3차

--- src/pages/boards/SearchPage.tsx
+++ src/pages/boards/SearchPage.tsx
@@ -1,4 +1,3 @@
-// src/pages/boards/SearchPage.tsx
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import PostList from "@components/Board/free/PostList";
