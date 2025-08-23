819acfc1c2f3090a626d34fa56a4dda61e4a09cc
Makee <ina0312@naver.com>
2025-08-23 17:08:37 +0900

    comment: TODO 추가(#119)

--- src/pages/boards/GithubListPage.tsx
+++ src/pages/boards/GithubListPage.tsx
@@ -1,4 +1,3 @@
-// src/pages/GithubListPage.tsx
 import { useCallback, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import GithubList from "@components/Board/github/GithubList";
