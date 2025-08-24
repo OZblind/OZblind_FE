ae105e839803152d346bd4aa50a6f1aa0055e637
L <lmwoo0327@naver.com>
2025-08-24 17:14:03 +0900

    feat: any 무시

--- src/features/posts/list/githubAdapter.tsx
+++ src/features/posts/list/githubAdapter.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { GithubListItem } from "@components/Board/github/GithubList";
 import type { PostListItem } from "@api/posts";
 
