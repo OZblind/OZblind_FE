85080315f0dee634015879b279160da77b836ebb
Makee <ina0312@naver.com>
2025-08-25 14:09:04 +0900

    feat: 읽지 않은 알림 있을 경우 알림 아이콘 옆에 활성화 점 표시되도록 함(#216)

--- src/components/Sidebar/SidebarFolded.tsx
+++ src/components/Sidebar/SidebarFolded.tsx
@@ -37,12 +37,12 @@ export function SidebarFolded({ onToggle }: { onToggle: () => void }) {
         />
         <button
           onClick={() => setNoticeOpen(true)}
-          className="p-2 rounded-full hover:bg-base-300"
+          className="relative p-2 rounded-full hover:bg-base-300"
         >
           <img src={notificationsIcon} alt="notificationsIcon" />
           {hasNew && (
             <i
-              className="absolute right-2 top-2 w-2 h-2 rounded-full bg-error"
+              className="absolute right-1.5 top-1.5 w-2 h-2 rounded-full bg-[#F43535] z-10"
               aria-hidden
             />
           )}
